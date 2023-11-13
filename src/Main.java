ea46fdcc8213f99114622ff9aeb2a1e0e5d990e2
Gulbeyaz <glbzbayram@gmail.com>
2023-11-13 13:57:20 +0300

    fourth method was created and runned by correct

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.*; //libraries as Scanner were imported
+
 public class Main {
     public static void main(String[] args) {
-       // System.out.println(shouldWakeUp(true,-1));
-      // System.out.println(hasTeen(23, 22, 42));
-       // System.out.println(isCatPlaying(true,10));
+        // System.out.println(shouldWakeUp(true,-1));
+        // System.out.println(hasTeen(23, 22, 42));
+        // System.out.println(isCatPlaying(true,10));
+        Scanner entry = new Scanner(System.in);
+        //double edge1 = entry.nextDouble();
+        //double edge2 = entry.nextDouble();
+        double radius = entry.nextDouble();
+        // System.out.println(area(edge1,edge2));
+        System.out.println(circleArea(radius));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int num) {
@@ -10,51 +19,69 @@ public class Main {
             return false;
         }
 
-         if (isBarking) {
+        if (isBarking) {
             if (num < 23 && num > 20 || num < 8 && num > 0) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
-        }
-         else {
-             return false;
-         }
-    }
-    public static boolean hasTeen(int... ages){
-        int isYoung=0;
-        for(int age: ages){
-
-          if(age >= 13 && age <= 19){
-              isYoung += 1;
-              break;
-          }
-        }
-        if(isYoung > 0 ){
-            return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public static boolean isCatPlaying(boolean season , int warmth){
+    public static boolean hasTeen(int... ages) {
+        int isYoung = 0;
+        for (int age : ages) {
 
-        if(season){
-            if(warmth >= 25 && warmth <=45 ){
-                return true;
+            if (age >= 13 && age <= 19) {
+                isYoung += 1;
+                break;
             }
-            else{
+        }
+        if (isYoung > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isCatPlaying(boolean season, int warmth) {
+
+        if (season) {
+            if (warmth >= 25 && warmth <= 45) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (warmth >= 25 && warmth <= 35) {
+                return true;
+            } else {
                 return false;
             }
         }
-        else {
-            if(warmth >= 25 && warmth <=35 ){
-                return true;
-            }
-            else{
-                return false;
+    }
+
+    public static double area(double... edges) {
+        double area = 1;
+        for (double edge : edges) {
+            if (edge < 0) {
+                return -1;
+            } else {
+                area = area * edge;
             }
         }
+        return area;
+    }
+
+    public static double circleArea(double radius) {
+        double area = 1;
+        if (radius < 0) {
+            return -1;
+        } else {
+            area = radius * radius * Math.PI;
+        }
+        return area;
     }
 }
